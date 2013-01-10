7cf52bf06ad3fbce403c8dda314d77037c900452
HamadaYusuke <hmsk.ultramarine@gmail.com>
2013-01-10 21:23:36 +0900

    Update CL_R01/CL_R1/CL_R01.c

--- CL_R01/CL_R1/CL_R01.c
+++ CL_R01/CL_R1/CL_R01.c
@@ -3,5 +3,7 @@
 void main(void) {
 	printf("Hey!");
     printf("Mr.Green.");
-    printf("Isn't the whereabouts of my lunch known? ")
+    printf("Isn't the whereabouts of my lunch known? ");
+    printf("----------------------------------------------");
+    printf("It is playing in my belly now. ")
 }
